1a826c5b676a9a7a91fddae10e59ebaf27f96ea2
rfriesz <57455239+rfriesz@users.noreply.github.com>
2022-03-04 19:32:46 -0800

    Update app_server.R

--- app_server.R
+++ app_server.R
@@ -1,13 +1,44 @@
-server <- function(input, output) {
+dat <-read.csv("https://raw.githubusercontent.com/rfriesz/info-201a-wi22-finalassignment-rfriesz/main/owid-co2-data.csv")
+ui <- fluidPage(theme = shinytheme("cerulean"),
+  navbarPage(
+    
+    "Climate Change Report",
   
-##  setwd('/Users/ryanf/Desktop/Data Science 201/info-201a-wi22-finalassignment-rfriesz...')
-##  data1 <- read.csv("owid-co2-data.csv")
-  
-  
-##  output$avg_2020_country_co2 <- renderPrint({
-##    filter(data, year == 2020) %>%
-##    summarize(data, co2, na.rm = TRUE) %>%
-##    results <- mean(co2)
-##    results  
-##  })
-}
+     
+   
+    tabPanel("Introductory Page",
+             mainPanel(
+               h1("Introduction"),
+               
+               h4("This project is about analyzing data regarding climate change, specifically co2 emissions. With this data set I will be able to create a visualization to draw focus to some of the problems it is causing. Exploring trends in co2 emissions from different countries and over time will reveal some of the major causes of climate change and hopefully spark ideas to improve the future"),
+              
+               h5("Average co2 across all countries and years"),
+               number <- mean(dat$co2, na.rm = TRUE),
+               textOutput(number),
+               
+               h5("Average co2 per capita across all countries and years"),
+               number <- mean(dat$co2_per_capita, na.rm = TRUE),
+               textOutput(number),
+               
+               h5("Average trade co2 across all countries and years"),
+               number <- mean(dat$trade_co2, na.rm = TRUE),
+               textOutput(number),
+               
+               h5("Average flaring co2 across all countries and years"),
+               number <- mean(dat$flaring_co2, na.rm = TRUE),
+               textOutput(number),
+               
+               
+               h5("Average oil co2 across all countries and years"),
+               number <- mean(dat$oil_co2, na.rm = TRUE),
+               textOutput(number),
+             )
+    ),
+    tabPanel("Visualization Page",
+        mainPanel(
+      
+        )
+    )
+  )
+)
+
